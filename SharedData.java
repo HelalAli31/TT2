48e8b0f98f76139c2f64d6da77998d0673272b06
HelalAli <helalali358@gmail.com>
2024-11-20 18:12:10 +0200

    SS

--- SharedData.java
+++ SharedData.java
@@ -3,7 +3,7 @@ public class SharedData
 	private int [] array;
 	private boolean [] winArray;
 	private boolean flag;
-	private final int b;
+	private final int sssssb;
 	
 	public SharedData(int[] array, int b) {
 		
